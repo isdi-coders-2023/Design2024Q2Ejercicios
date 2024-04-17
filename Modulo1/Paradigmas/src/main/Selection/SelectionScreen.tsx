2ecebd3c5e0cb94d90a28c3ba07cc4ebf4e5762d
Daniel Morán <dmoranj@gmail.com>
2024-04-18 00:25:57 +0200

    Implement the story selection screen

--- Modulo1/Paradigmas/src/main/Selection/SelectionScreen.tsx
+++ Modulo1/Paradigmas/src/main/Selection/SelectionScreen.tsx
@@ -1,12 +1,19 @@
+import basic1 from "../../stories/basic.story.json";
+import basic2 from "../../stories/basic2.story.json";
 import basic3 from "../../stories/basic3.story.json";
 import { Story } from "../../model/stories";
 import styles from "./SelectionScreen.module.scss";
+import { useState } from "react";
+
+const stories = [basic1, basic2, basic3] as Story[];
 
 interface SelectionScreenProps {
   start: (story: Story) => void;
 }
 
 export const SelectionScreen: React.FC<SelectionScreenProps> = ({ start }) => {
+  const [selected, setSelected] = useState<Story | null>(null);
+
   const startStory = () => {
     start(basic3 as Story);
   };
@@ -15,20 +22,25 @@ export const SelectionScreen: React.FC<SelectionScreenProps> = ({ start }) => {
     <>
       <section className={styles.selectionScreen}>
         <ul className={styles.availableStories}>
-          <li className={`${styles.story}`}>Una de las historias</li>
-          <li className={`${styles.story} ${styles.selected}`}>
-            Una de las historias
-          </li>
-          <li className={`${styles.story}`}>Una de las historias</li>
+          {stories.map((story) => (
+            <li
+              className={`${styles.story} ${
+                selected?.title === story.title ? styles.selected : ""
+              }`}
+              key={story.title}
+              onClick={() => setSelected(story)}
+            >
+              {story.title}
+            </li>
+          ))}
         </ul>
         <p className={styles.storyDescription}>
-          Esta debería ser la descripción de la historia cuando el usuario clica
-          en ella
+          {selected ? selected.initialDescription : "Selecciona una historia"}
         </p>
       </section>
       <section className={styles.controls}>
-        <button className={styles.comenzar} onClick={startStory}>
-          Comenzar
+        <button className={`${styles.comenzar} `} onClick={startStory}>
+          {selected ? "Comenzar" : "Selecciona una historia para comenzar"}
         </button>
       </section>
     </>
